3cb2cc50c6fb2189fa6f07c580fd4832fe0a10d2
nogwee <144659085+nogwee@users.noreply.github.com>
2024-09-04 23:11:15 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,16 +4,20 @@ from streamlit_folium import st_folium
 import folium
 import pandas as pd
 
+st.set_page_config( 
+    layout="wide"
+)
+
 #basin = tracks.TrackDataset(basin="west_pacific", source="ibtracs")
 #storm = basin.get_storm(("jebi", 2018))
 #df = storm.to_dataframe()
 #df.to_csv("test.csv")
 df = pd.read_csv("test.csv")
+df = df.dropna(how="all", axis=1).drop(df.columns[0], axis=1)
 
 m = folium.Map(location=[30, 145], zoom_start=4, tiles='cartodbpositron')
-
-# for idx, row in df.iterrows():
-#     folium.Marker([row["lat"], row["lon"]], popup=row["time"]).add_to(m)
+minimap = folium.plugins.MiniMap(tile_layer='cartodbpositron',toggle_display=True)
+m.add_child(minimap)
 
 folium.PolyLine(
     locations=[(row['lat'], row['lon']) for idx, row in df.iterrows()],
@@ -22,4 +26,15 @@ folium.PolyLine(
     opacity=1
 ).add_to(m)
 
-st_folium(m)
+
+for idx, row in df.iterrows():
+    jst = pd.to_datetime(row.time).tz_localize("utc").tz_convert("Asia/Tokyo")
+    folium.Circle([row["lat"], row["lon"]], radius=20000, tooltip=f"{jst:%Y年%m月%d日%H時%M分}", fill=True).add_to(m)
+
+# for idx, row in df.iterrows():
+#     folium.CircleMarker([row["lat"], row["lon"]], color="black", radius=4, fill_color="black", opacity=0, tooltip=f"{jst:%Y年%m月%d日%H時%M分}").add_to(m)
+
+
+st_folium(m, use_container_width=True, height=720, returned_objects=[])
+
+df
