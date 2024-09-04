240287a756fab42e65bf00a3bda9a5ad05a695b1
nogwee <144659085+nogwee@users.noreply.github.com>
2024-09-05 01:18:24 +0900

    Add files via upload

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from streamlit_folium import st_folium
 import folium
 import pandas as pd
 from folium import FeatureGroup, LayerControl
+import geopandas as gpd
 
 st.set_page_config( 
     layout="wide"
@@ -19,6 +20,7 @@ map_col, menu_col = placeholder.columns([4, 1])
 df = pd.read_csv("test.csv")
 df = df.dropna(how="all", axis=1).drop(df.columns[0], axis=1)
 
+gdf = gpd.read_file("N02-23_GML/N02-23_RailroadSection.shp")
 
 m = folium.Map(
     location=[30, 145], 
@@ -37,6 +39,7 @@ m.add_child(minimap)
 with map_col:
 
     keiro = FeatureGroup(name="経路図")
+    rosen = FeatureGroup(name="鉄道")
 
     # if on:
     folium.PolyLine(
@@ -55,9 +58,15 @@ with map_col:
     # for idx, row in df.iterrows():
     #     folium.CircleMarker([row["lat"], row["lon"]], color="black", radius=4, fill_color="black", opacity=0, tooltip=f"{jst:%Y年%m月%d日%H時%M分}").add_to(m)
 
+    tooltip = folium.GeoJsonTooltip(fields=["N02_004", "N02_003"],aliases=["会社:", "路線名:"],)
+    folium.GeoJson(gdf, tooltip=tooltip).add_to(rosen)
+
+    rosen.add_to(m)
     keiro.add_to(m)
+
     LayerControl().add_to(m)
 
     st_folium(m, use_container_width=True, height=720, returned_objects=[])
 
     df
+    gdf
